611153af8b1e3dc933fbc8970c075e78f8666c99
elhajuojy <souirimehdi311@gmail.com>
2022-07-15 14:10:31 +0100

    news controller

--- lib/controller/news_screen_controller.dart
+++ lib/controller/news_screen_controller.dart
@@ -1,14 +1,26 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 import 'package:newapp_/model/article.dart';
 
 class NewsController extends GetxController {
+  IconData favIcon = Icons.favorite_border_outlined;
+
+  IconFaveChanger() {
+    favIcon = favIcon == Icons.favorite_border
+        ? Icons.favorite_border_outlined
+        : Icons.favorite_border;
+    print("need to change");
+    print(favIcon.toString());
+    update();
+  }
+
   Future<List<Article>> getArticles() async {
     var url = Uri.parse(
-        'https://newsapi.org/v2/everything?q=apple&from=2022-06-15&sortBy=publishedAt&apiKey=9ff4fdd6066649d1bba339f2b449fb29');
+        'https://newsapi.org/v2/everything?q=tesla&from=2022-06-15&sortBy=publishedAt&apiKey=9ff4fdd6066649d1bba339f2b449fb29');
     http.Response res = await http.get(url);
     if (res.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(res.body);
